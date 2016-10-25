b540a1655b0207bf89b6cb5b8ff448582a2bf283
David East <deast@google.com>
2016-10-25 06:06:47 -0700

    feat(events): Select events based on city indexes

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,33 +1,72 @@
 /// <reference path="../node_modules/firebase/firebase.d.ts" />
 (function() {
 
-  const container = document.getElementById('app-shell-events');
-  const eventsTemplate = window['Handlebars'].templates['events'];
-  const app = initApp();
-  listenForEvents(app);
-
   function initApp() {
     const config = {
-      apiKey: "AIzaSyBAjWCgQbbhhwoy4TffyTmHEMkhWG3dRzE",
-      authDomain: "sql-for-firebase-devs.firebaseapp.com",
-      databaseURL: "https://sql-for-firebase-devs.firebaseio.com",
-      storageBucket: "sql-for-firebase-devs.appspot.com",
-      messagingSenderId: "583164206075"
+      apiKey: "AIzaSyCBOqlnA5BKfGh_T2o35deqTbI2dujITA4",
+      authDomain: "bonfires-8c02c.firebaseapp.com",
+      databaseURL: "https://bonfires-8c02c.firebaseio.com",
+      storageBucket: "bonfires-8c02c.appspot.com",
+      messagingSenderId: "367947366674"
     };
     return firebase.initializeApp(config);
   }
 
-  function listenForEvents(app: firebase.app.App) {
-    const eventsRef = app.database().ref('events').limitToFirst(10);
-    eventsRef.on('child_added', snap => {
-      const event = snap.val() as Event;
-      renderTemplate(event);
-    });
+  class App {
+    container: HTMLElement;
+    locationNav: HTMLElement;
+    eventsTemplate: Function;
+    activeRef: firebase.database.Reference;
+    activeListener: any;
+    rootRef: firebase.database.Reference;
+
+    constructor(public firebaseApp: firebase.app.App) {
+      this.rootRef = this.firebaseApp.database().ref();
+      this.eventsTemplate = window['Handlebars'].templates['events'];
+
+      this.attachDOMListeners = this.attachDOMListeners.bind(this);
+      this.getDOMElements = this.getDOMElements.bind(this);
+      this.renderTemplate = this.renderTemplate.bind(this);
+      this.listenForEvents = this.listenForEvents.bind(this);
+
+      this.getDOMElements();
+      this.attachDOMListeners();
+    }
+
+    getDOMElements() {
+      this.container = document.getElementById('dynamic-events');
+      this.locationNav = document.getElementById('location-nav');
+    }
+
+    attachDOMListeners() {
+
+      this.locationNav.addEventListener('click', e => {
+        const anchor = e.srcElement;
+        const location = e.srcElement.attributes.getNamedItem('data-location').nodeValue;
+        this.activeRef = firebase.database().ref().child('locationEvents').child(location);
+        this.container.innerHTML = '';
+        this.listenForEvents();
+      });
+
+    }
+
+    renderTemplate(event) {
+      const compiledEvent = this.eventsTemplate(event);
+      this.container.innerHTML += compiledEvent;
+    }
+
+    listenForEvents() {
+      if (this.activeListener) {
+        this.activeRef.off('child_added', this.activeListener);        
+      }
+      this.activeListener = this.activeRef.on('child_added', snap => {
+        this.renderTemplate(snap.val());
+      });
+    }
+
   }
 
-  function renderTemplate(event: Event) {
-    const compiledEvent = eventsTemplate(event);
-    container.innerHTML += compiledEvent;
-  }
+  const firebaseApp = initApp();
+  new App(firebaseApp);
 
 })();
